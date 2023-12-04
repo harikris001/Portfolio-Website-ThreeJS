03cb4c7c18c3c6d19342e38f1ac878b76afd13a3
harikris001 <hk5983835@gmail.com>
2023-12-04 18:36:08 +0530

    Setup Home Page with 3d assets

---
+++ src/pages/Home.jsx
@@ -0,0 +1,85 @@
+import { Suspense, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import Loader from "../components/Loader";
+import Island from "../models/Island";
+import Sky from "../models/Sky";
+import Bird from "../models/Bird";
+import Plane from "../models/Plane";
+import Homeinfo from "../components/Homeinfo";
+
+
+const Home = () => {
+  const [isRotating, setIsRotating] = useState(false);
+  const [currentStage, setCurrentStage] = useState(1)
+
+  const adjustIslandForScreenSize = () => {
+    let screensScale = null;
+    let screenPosition;
+    let rotation = [0.1, 4.7, 0];
+    if (window.innerWidth < 768) {
+      screensScale = [0.9, 0.9, 0.9];
+      screenPosition = [0, -6.5, -43.4];
+    } else {
+      screensScale = [1, 1, 1];
+      screenPosition = [0, -6.5, -43.4];
+    }
+    return [screensScale, screenPosition, rotation];
+  };
+
+  const adjustPlanelandForScreenSize = () => {
+    let screensScale, screenPosition;
+    if (window.innerWidth < 768) {
+      screensScale = [1.5, 1.5, 1.5];
+      screenPosition = [0, -1.5, 0];
+    } else {
+      screensScale = [3, 3, 3];
+      screenPosition = [0, -4, -4];
+    }
+    return [screensScale, screenPosition];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
+  const [planeScale, planePosition] = adjustPlanelandForScreenSize();
+
+  return (
+    <section className="w-full h-screen relative">
+      <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
+        {currentStage && <Homeinfo currentStage={currentStage} />}
+      </div>
+      <Canvas
+        className={`w-full h-screen bg-transparent ${isRotating ? "cursor-grabbing" : "cursor-grab"
+          }`}
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight
+            skyColor="#b1e1ff"
+            groundColor="#000000"
+            intensity={1}
+          />
+          <Bird />
+          <Sky isRotating={isRotating} />
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
+            setCurrentStage={setCurrentStage}
+          />
+          <Plane
+            isRotating={isRotating}
+            planeScale={planeScale}
+            planePosition={planePosition}
+            rotation={[0, 20, 0]}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
+
+export default Home;
