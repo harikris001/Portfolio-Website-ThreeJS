8ef5ad5e60c1cb5d62e642b50241674befd5b190
harikris001 <hk5983835@gmail.com>
2023-12-04 18:37:26 +0530

    About page and Project page intial commit

---
+++ src/pages/About.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='max-container'>
+      <h1 className='head-text'>Hello</h1>
+    </section >
+  )
+}
+
+export default About
