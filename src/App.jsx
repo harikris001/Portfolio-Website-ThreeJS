82da75b44ab017a0af2148a9ea465ba1a70b43aa
harikris001 <hk5983835@gmail.com>
2023-12-05 22:41:45 +0530

    Added Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import {Home, About, Projects, Contact } from './pages'
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
                     <Route path="/projects" element={<Projects />} />
                     <Route path="/contact" element={<Contact />} />
                 </Routes>
+                <Footer />
             </Router>
         </main>
     )
